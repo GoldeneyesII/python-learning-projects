37aa6b8080317318279bef46016ae1c1c8e9e044
GoldeneyesII <22719469+GoldeneyesII@users.noreply.github.com>
2025-11-30 00:33:42 -0800

    Reset characters to using Energy. Corrected typo. Changing match/case to if/else for comparisons.

--- project-02-rpg-game/character.py
+++ project-02-rpg-game/character.py
@@ -8,7 +8,7 @@ class Serializable:
         pass
 
 class Character(Serializable, ABC):
-    def __init__(self, name, health, attack, defense, magic_atk, magic_def, mana=0):
+    def __init__(self, name, health, attack, defense, magic_atk, magic_def, energy=0):
         self.name = name
         self.max_health = 100
         self.health = health
@@ -18,13 +18,12 @@ class Character(Serializable, ABC):
         self.defense = defense
         self.magic_atk = magic_atk
         self.magic_def = magic_def
-        self.max_mana = mana
-        self.mana = mana
+        self.max_energy = energy
+        self.energy = energy
         self.level = 1
         self.experience = 0
         self.physical_resistance = 0
         self.magical_resistance = 0
-        self.auxiliary = "mana" #Can change for future things such as "energy" or "rage" depending on class. For now, only Mana.
 
     def take_damage(self, damage):
         # Calculate damage after defense
@@ -48,14 +47,14 @@ class Character(Serializable, ABC):
         pass
 
     def __str__(self):
-        return f"{self.__class__.__name__}: {self.name} (HP: {self.health}/{self.max_health}, MP: {self.mana}/{self.max_mana})"
+        return f"{self.__class__.__name__}: {self.name} (HP: {self.health}/{self.max_health}, Energy: {self.energy}/{self.max_energy})"
 
     def aux_check(self, required):
-        return True if self.auxiliary >= required else print(f"Not enough {self.auxiliary}")
+        return True if self.energy >= required else print(f"Not enough energy")
 
 class Warrior(Character):
-    def __init__(self, name, health, attack, defense, mana):
-        super().__init__(name, health, attack, defense, mana)
+    def __init__(self, name, health, attack, defense, energy):
+        super().__init__(name, health, attack, defense, attack, defense, energy)
         self.physical_resistance += 10 #Warriors start with physical resistance to disarms.
 
 
@@ -71,34 +70,34 @@ class Warrior(Character):
         if target is None or not target.is_alive():
             #Optional - disable the ability to select this as an option.
             return "Invalid target"
-        
-        if self.mana < 1:
-            #Optional - disable the ability to select this as an option.
-            return "Not enough mana"
 
-        self.mana -= 1
+        if self.energy < 1:
+            #Optional - disable the ability to select this as an option.
+            return "Not enough energy"
+
+        self.energy -= 1
         # TODO: Implement full disarm when equipment system is ready.
         
-        print(f"{self.name} attempts ot disarm {target.name}!")
+        print(f"{self.name} attempts to disarm {target.name}!")
         disarm_val = random.randint(1,20) #Assigning to variable so value can be logged at a later time for debugging.
         return f"Disarm was successful!" if disarm_val > target.resist else f"Disarm failed!"
 
 class Mage(Character):
-    def __init__(self, name, health, attack, defense, mana):
-        super().__init__(name, health, attack, defense, mana=100)
+    def __init__(self, name, health, attack, defense, energy):
+        super().__init__(name, health, attack, defense, attack, defense, energy=100)
         self.magical_resistance += 10
 
     def special_ability(self, target=None):
         """
         Drain - drains the target of life, gaining a percentage back as HP
         """
-        
+
         min_heal, max_heal = 4, 12
         cost = 5
-        if aux_check(cost):
+        if self.aux_check(cost):
             if target is None or not target.is_alive():
                 return "Invalid target"
-            self.mana -= cost
+            self.energy -= cost
             
             #Attempt magical attack against target
             damage = random.randint(1,20) + (self.magic_atk - target.__magic_def)
@@ -110,8 +109,8 @@ class Mage(Character):
 
 
 class Rogue(Character):
-    def __init__(self, name, health, attack, defense, mana):
-        super().__init__(name, health, attack, defense, mana)
+    def __init__(self, name, health, attack, defense, energy):
+        super().__init__(name, health, attack, defense, attack, defense, energy)
 
     def special_ability(self, target):
         """
@@ -120,28 +119,28 @@ class Rogue(Character):
 
         cost = 2
 
-        if aux_check(cost):
+        if self.aux_check(cost):
             if target is None or not target.is_alive():
                 return "Invalid target"
-            self.mana -= cost
+            self.energy -= cost
 
             #Only succeed if self.attack - target.defense > 0
             #Be more effective based on how much stronger the attack is
-            match (self.attack - target.defense):
-                case <= 0:
-                    weaken = 0
-                case <= 5:
-                    weaken = random.randint(1,3)
-                case <= 10:
-                    weaken = random.randint(2,5)
-                case <= 20:
-                    weaken = random.randint(4,8)
-                default:
-                    weaken = 9
+            difference = self.attack - target.defense
+            if difference <= 0:
+                weaken = 0
+            elif difference <= 5:
+                weaken = random.randint(1,3)
+            elif difference <= 10:
+                weaken = random.randint(2,5)
+            elif difference <= 20:
+                weaken = random.randint(4,8)
+            else:
+                weaken = 9
 
-                if weaken == 0:
-                    return "You strike at their kidneys, but they are not effected!"
+            if weaken == 0:
+                return "You strike at their kidneys, but they are not effected!"
 
-                target.attack -= weaken
-                target.defense -= weaken
-                return f"You strike the targets kidneys and weaken them by {weaken} points!"
+            target.attack -= weaken
+            target.defense -= weaken
+            return f"You strike the targets kidneys and weaken them by {weaken} points!"
